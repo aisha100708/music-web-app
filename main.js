7faee771655acad267fcfb1f53a84565385b1091
aisha100708 <71584539+aisha100708@users.noreply.github.com>
2022-01-11 08:58:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 //song1=music=harrypotter song2=music2=peterpan
 song1 = "";
 song2 = "";
+leftWrist_x  = 0;
+leftWrist_y = 0;
+rightWrist_x = 0;
+rightWrist_y = 0;
 function preload() {
     song1 = loadSound("");
     song2 = loadSound("");
@@ -10,7 +14,25 @@ function setup() {
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotPoses);
 }
 function draw() {
     image(video, 0, 0, 600, 400);
+}
+function modelLoaded() {
+    console.log("Posenet is initialized!");
+}
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftWrist_x = results[0].pose.leftWrist.x;
+        leftWrist_y = results[0].pose.leftWrist.y;
+        rightWrist_x = results[0].pose.rightWrist.x;
+        rightWrist_y = results[0].pose.rightWrist.y;
+        console.log("leftWrist_x = " + leftWrist_x);
+        console.log("leftWrist_y = " + leftWrist_y);
+        console.log("rightWrist_x = " + rightWrist_x);
+        console.log("rightWrist_y = " + rightWrist_y);
+    }
 }
